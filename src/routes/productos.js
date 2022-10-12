dd3d89bc10cded1cf05367f0af2a26622672149f
sebastian m <sebastianminotti@gmail.com>
2022-10-11 23:07:42 -0300

    commit 11-10-2022

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 const router = Router();
 
-import {all, single, filtrarProductos } from "../controllers/productos.js";
+import { all, single, filtrarProductos } from "../controllers/productos.js";
+import { all as allCat } from "../controllers/admin/categoriasProd.js";
 
 router.get("/single/:id", single);
-router.get("/",all);
+router.get("/", all);
 
 router.get("/categoria/:categoria", filtrarProductos);
 
